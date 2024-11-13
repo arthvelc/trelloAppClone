13925d25333bdd9da597a6437b46a45ed5b5960f
Arturo Solares <asolares.devml@gmail.com>
2024-11-12 23:36:34 -0600

    feat(login): mejoras en la interfaz de usuario del formulario de login e integración de Tailwind Forms
    - login.component.html: ajustes en el diseño del formulario de login para mejor compatibilidad con diferentes tamaños de pantalla.
    - login.component.ts: actualización de imágenes de fondo y logo de Trello.
    - package.json y package-lock.json: se añade el paquete @tailwindcss/forms para estilos de formularios.
    - tailwind.config.js: inclusión del plugin @tailwindcss/forms en la configuración de TailwindCSS.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,5 +9,5 @@ import { Component, signal } from '@angular/core';
 export default class LoginComponent {
   imgRight = signal('https://res.cloudinary.com/djp3pbhfg/image/upload/v1729884068/udrawright_tt3u7g.webp');
   imgLeft = signal('https://res.cloudinary.com/djp3pbhfg/image/upload/v1729884068/udrawleft_twnna6.webp');
-  imgTrello = signal('https://res.cloudinary.com/djp3pbhfg/image/upload/v1729884067/trelloLogo_kccptn.webp');
+  imgTrello = signal('https://res.cloudinary.com/djp3pbhfg/image/upload/v1729884067/trelloLogo_kccptn.webp')
 }
